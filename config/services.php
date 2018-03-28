5635c3a07cd33c699ce88c5be1bcf31f9af48000
ghwolf <g470756541@gmail.com>
2018-03-28 06:28:45 +0000

    Auto translate slug

--- config/services.php
+++ config/services.php
@@ -35,4 +35,15 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'baidu_translate' => [
+        'appid' => env('BAIDU_TRANSLATE_APPID'),
+        'key'   => env('BAIDU_TRANSLATE_KEY'),
+    ],
+
+    'weixin' => [
+        'client_id' => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect' => env('WEIXIN_REDIRECT_URI'),
+    ],
+
 ];

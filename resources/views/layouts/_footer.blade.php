2c3e74da7a4bb8bdbe0d9130d37f16729467441c
ghwolf <g470756541@gmail.com>
2018-03-28 09:20:56 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://github.com/Ghwolfzx" target="_blank">Ghwolf</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:ghwolf@foxmail.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>

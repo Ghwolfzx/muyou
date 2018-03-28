335ef634f21a84bc2260d69df210c945e0db623e
ghwolf <g470756541@gmail.com>
2018-03-28 05:51:00 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - 牧游内部展示</title>
+    <title>@yield('title', '牧游科技') - 牧游内部展示</title>
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">

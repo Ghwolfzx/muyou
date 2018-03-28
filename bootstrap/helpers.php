41de3e9da25796ff53ae5777fed0298760c498f7
ghwolf <g470756541@gmail.com>
2018-03-28 03:46:41 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,6 +1,14 @@
 <?php
 
+// 根据浏览器返回类名
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
+}
+
+// 对内容进行处理
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
 }
